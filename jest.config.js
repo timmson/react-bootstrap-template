668bb5942f9510960316b7fdf75e2955d9da4942
timmson <timmson666@mail.ru>
2022-02-18 14:30:01 +0300

    Refactor React App

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/*"],
     coverageReporters: ["lcov"],
     testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"]
 };
